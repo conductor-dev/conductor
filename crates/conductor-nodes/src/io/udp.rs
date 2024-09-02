26156dcbfa46d80609d09389357b0c6007db4c12
JabobKrauskopf <mail@jakobkraus.de>
2024-09-02 22:25:51 +0200

    feat: UdpSender

--- crates/conductor-nodes/src/io/udp.rs
+++ crates/conductor-nodes/src/io/udp.rs
@@ -1,5 +1,5 @@
 use conductor_core::{
-    ports::{NodeConfigOutputPort, NodeRunnerOutputPort},
+    ports::{NodeConfigInputPort, NodeConfigOutputPort, NodeRunnerInputPort, NodeRunnerOutputPort},
     NodeConfig, NodeRunner,
 };
 use std::net::UdpSocket;
@@ -7,6 +7,10 @@ use std::net::UdpSocket;
 pub trait UdpDeserializer {
     fn max_packet_size() -> usize;
     fn deserialize_packet(bytes: &[u8]) -> Self;
+}
+
+pub trait UdpSerializer {
+    fn serialize_packet(self) -> Vec<u8>;
 }
 
 struct UdpReceiverRunner<T: Clone + UdpDeserializer> {
@@ -26,15 +30,33 @@ impl<T: Clone + UdpDeserializer> NodeRunner for UdpReceiverRunner<T> {
     }
 }
 
+struct UdpSenderRunner<T: UdpSerializer> {
+    socket: UdpSocket,
+    send_addr: String,
+    input: NodeRunnerInputPort<T>,
+}
+
+impl<T: UdpSerializer> NodeRunner for UdpSenderRunner<T> {
+    fn run(self: Box<Self>) {
+        loop {
+            let data = self.input.recv().unwrap();
+
+            let buffer = data.serialize_packet();
+
+            self.socket.send_to(&buffer, &self.send_addr).unwrap();
+        }
+    }
+}
+
 pub struct UdpReceiver<'a, T: Clone + UdpDeserializer> {
-    addr: &'a str,
+    bind_addr: &'a str,
     pub output: NodeConfigOutputPort<T>,
 }
 
 impl<'a, T: Clone + UdpDeserializer> UdpReceiver<'a, T> {
-    pub fn new(addr: &'a str) -> Self {
+    pub fn new(bind_addr: &'a str) -> Self {
         Self {
-            addr,
+            bind_addr,
             output: NodeConfigOutputPort::<T>::new(),
         }
     }
@@ -43,11 +65,39 @@ impl<'a, T: Clone + UdpDeserializer> UdpReceiver<'a, T> {
 // TODO: Can + Send + 'static be removed?
 impl<'a, T: Clone + UdpDeserializer + Send + 'static> NodeConfig for UdpReceiver<'a, T> {
     fn into_runner(self: Box<Self>) -> Box<dyn NodeRunner + Send> {
-        let socket = UdpSocket::bind(self.addr).unwrap();
+        let socket = UdpSocket::bind(self.bind_addr).unwrap();
 
         Box::new(UdpReceiverRunner {
             socket,
             output: self.output.into(),
+        })
+    }
+}
+
+pub struct UdpSender<'a, T: UdpSerializer> {
+    bind_addr: &'a str,
+    send_addr: &'a str,
+    pub input: NodeConfigInputPort<T>,
+}
+
+impl<'a, T: UdpSerializer> UdpSender<'a, T> {
+    pub fn new(bind_addr: &'a str, send_addr: &'a str) -> Self {
+        Self {
+            bind_addr,
+            send_addr,
+            input: NodeConfigInputPort::new(),
+        }
+    }
+}
+
+impl<'a, T: UdpSerializer + Send + 'static> NodeConfig for UdpSender<'a, T> {
+    fn into_runner(self: Box<Self>) -> Box<dyn NodeRunner + Send> {
+        let socket = UdpSocket::bind(self.bind_addr).unwrap();
+
+        Box::new(UdpSenderRunner {
+            socket,
+            send_addr: self.send_addr.to_string(),
+            input: self.input.into(),
         })
     }
 }
