ab49b54be6911b0b9753126e8b51e6de14d72838
JabobKrauskopf <mail@jakobkraus.de>
2024-09-30 22:13:51 +0200

    feat: add FileReader and FileWriter

--- crates/conductor-cli/src/main.rs
+++ crates/conductor-cli/src/main.rs
@@ -2,80 +2,30 @@ use conductor::prelude::*;
 use std::fmt::Display;
 
 fn main() {
-    // let sample_generator = SampleGenerator::<MyPacket>::new(31250);
-    // let normer = Normer::new();
+    let file_reader = FileReader::<MyPacket>::new("./output.csv");
+    let lambda = Lambdaer::new(|x: MyPacket| x.0);
+    let audio_player = AudioPlayer::new();
 
-    let recorder = AudioRecorder::new();
-    let player = AudioPlayer::new();
+    file_reader.output.connect(&lambda.input);
+    lambda.output.connect(&audio_player.input);
 
-    let immediate = Immediate::new(5.0);
-    let multiply = Multiplier::new();
-
-    let downsampler = Downsampler::new(5000);
-    let console_printer = ConsolePrinter::new();
-    let lambda = Lambdaer::new(|x: f32| MyPacket(x));
-    let udp_sender = UdpSender::new("127.0.0.1:0", "127.0.0.1:9090");
-
-    let fft = FFT::new(1024);
-    let inverse_fft = InverseFFT::new(1024);
-
-    immediate.output.connect(&multiply.input1);
-    recorder.output.connect(&multiply.input2);
-
-    multiply.output.connect(&downsampler.input);
-    downsampler.output.connect(&console_printer.input);
-
-    multiply.output.connect(&fft.input);
-    fft.output.connect(&inverse_fft.input);
-
-    inverse_fft.output.connect(&lambda.input);
-    inverse_fft.output.connect(&player.input);
-    lambda.output.connect(&udp_sender.input);
-
-    pipeline![
-        downsampler,
-        recorder,
-        player,
-        lambda,
-        console_printer,
-        udp_sender,
-        fft,
-        inverse_fft,
-        multiply,
-        immediate
-    ]
-    .run();
+    pipeline![file_reader, audio_player, lambda].run();
 }
 
-#[derive(Clone, Copy, Norm)]
+#[derive(Clone)]
 struct MyPacket(f32);
+
+impl From<String> for MyPacket {
+    fn from(s: String) -> Self {
+        MyPacket(s.parse().unwrap())
+    }
+}
 
 impl Sample for MyPacket {
     fn sample(sample_rate: usize, current_sample: usize) -> Self {
         MyPacket(f32::sample(sample_rate, current_sample))
     }
 }
-
-impl Sin for MyPacket {
-    type Output = Self;
-
-    fn sin(self) -> Self {
-        MyPacket(self.0.sin())
-    }
-}
-
-impl UdpSerializer for MyPacket {
-    fn serialize_packet(self) -> Vec<u8> {
-        self.0.to_ne_bytes().to_vec()
-    }
-}
-
-// impl UdpSerializer for MyPacket {
-//     fn serialize_packet(self) -> Vec<u8> {
-//         self.0.to_ne_bytes().to_vec()
-//     }
-// }
-
 impl Display for MyPacket {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(f, "{}", self.0)
---
+++ crates/conductor-nodes/src/io/file.rs
@@ -0,0 +1,95 @@
+use conductor_core::{
+    ports::{NodeConfigInputPort, NodeConfigOutputPort, NodeRunnerInputPort, NodeRunnerOutputPort},
+    NodeConfig, NodeRunner,
+};
+use std::{
+    fmt::Display,
+    fs::OpenOptions,
+    io::{Read, Write},
+    path::{Path, PathBuf},
+};
+
+struct FileReaderRunner<T: From<String> + Clone> {
+    output: NodeRunnerOutputPort<T>,
+    path: PathBuf,
+}
+
+impl<T: From<String> + Clone> NodeRunner for FileReaderRunner<T> {
+    fn run(self: Box<Self>) {
+        let mut file = OpenOptions::new().read(true).open(self.path).unwrap();
+        let mut contents = String::new();
+        file.read_to_string(&mut contents).unwrap();
+        let lines = contents.lines();
+
+        for line in lines {
+            self.output.send(&line.to_string().into());
+        }
+    }
+}
+
+pub struct FileReader<T: From<String> + Clone> {
+    pub output: NodeConfigOutputPort<T>,
+    path: PathBuf,
+}
+
+impl<T: From<String> + Clone> FileReader<T> {
+    pub fn new<P: AsRef<Path>>(path: P) -> Self {
+        Self {
+            output: NodeConfigOutputPort::<T>::new(),
+            path: path.as_ref().to_path_buf(),
+        }
+    }
+}
+
+// TODO: Can + Send + 'static be removed?
+impl<T: From<String> + Clone + Send + 'static> NodeConfig for FileReader<T> {
+    fn into_runner(self: Box<Self>) -> Box<dyn NodeRunner + Send> {
+        Box::new(FileReaderRunner {
+            output: self.output.into(),
+            path: self.path,
+        })
+    }
+}
+
+struct FileWriterRunner<T: Display> {
+    input: NodeRunnerInputPort<T>,
+    path: PathBuf,
+}
+
+impl<T: Display> NodeRunner for FileWriterRunner<T> {
+    fn run(self: Box<Self>) {
+        let mut file = OpenOptions::new()
+            .create(true)
+            .append(true)
+            .open(self.path)
+            .unwrap();
+
+        loop {
+            writeln!(file, "{}", self.input.recv().unwrap()).unwrap();
+        }
+    }
+}
+
+pub struct FileWriter<T: Display> {
+    pub input: NodeConfigInputPort<T>,
+    path: PathBuf,
+}
+
+impl<T: Display> FileWriter<T> {
+    pub fn new<P: AsRef<Path>>(path: P) -> Self {
+        Self {
+            input: NodeConfigInputPort::<T>::new(),
+            path: path.as_ref().to_path_buf(),
+        }
+    }
+}
+
+// TODO: Can + Send + 'static be removed?
+impl<T: Display + Send + 'static> NodeConfig for FileWriter<T> {
+    fn into_runner(self: Box<Self>) -> Box<dyn NodeRunner + Send> {
+        Box::new(FileWriterRunner {
+            input: self.input.into(),
+            path: self.path,
+        })
+    }
+}
